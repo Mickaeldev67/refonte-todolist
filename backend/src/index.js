f81c8dd33dcd31bcc9b50da355a1c98f6b34c987
Mickael Joly <jolymickael67340@gmail.com>
2026-02-07 09:31:56 +0100

    chore : Séparer le back et le front Séparation des fichier static vers un dossier frontend et modification du pointeur express.static dans l'index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,9 +5,10 @@ const getItems = require('./routes/getItems');
 const addItem = require('./routes/addItem');
 const updateItem = require('./routes/updateItem');
 const deleteItem = require('./routes/deleteItem');
+const path = require('path');
 
 app.use(express.json());
-app.use(express.static(__dirname + '/static'));
+app.use(express.static(path.join(__dirname, '../../frontend')));
 
 app.get('/items', getItems);
 app.post('/items', addItem);
