bf1fdcf5a153b5c7aa7b806d52dbfa1b932a7ef0
Mickael Joly <jolymickael67340@gmail.com>
2026-02-21 10:31:38 +0100

    feat: Ajouter un test interdisant sqlite3 en test

--- backend/dependency-cruiser.config.js
+++ backend/dependency-cruiser.config.js
@@ -6,5 +6,12 @@ module.exports = {
       from: { path: '^src/domain' },
       to: { path: '^src/persistence' },
     },
+    {
+      name: 'no-sqlite-in-tests',
+      comment: 'Tests must not use sqlite',
+      severity: 'error',
+      from: { path: '^spec' },
+      to: { path: 'sqlite3' }
+    },
   ],
 };
